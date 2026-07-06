87a5f6c4f694a5fedee123471c3071f38f33158d
Siddhaviraul22 <raulsiddhavi678@gmail.com>
2026-07-07 02:30:06 +0530

    Day 1 : Project planning, requirement analysis, UI wireframes, Git repository setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-900">
+        AI Code Review Assistant
+      </h1>
+    </div>
+  );
+}
+
+export default App;
